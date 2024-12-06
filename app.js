ee4117ca98e9e09f8f2db5aed0200bff19826040
sebasraven <ssb2841@outlook.es>
2024-12-06 01:24:55 -0600

    Creando proyecto

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const oracledb = require('oracledb');
+
+// Configuración de Express
+const app = express();
+const port = 3000;
+
+// Middleware para parsear JSON en el cuerpo de las solicitudes
+app.use(bodyParser.json());
+app.use(express.static('public'));  // Para servir archivos estáticos (HTML, CSS, JS)
